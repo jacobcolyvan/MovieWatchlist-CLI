d3eca842477c40e82b9789c8938f50089a381b8f
Jacob Colyvan <jacob_colyvan@keemail.me>
2020-03-26 11:24:42 +1100

    minor changes

---
+++ control.rb
@@ -0,0 +1,28 @@
+
+class Movie
+    attr_accessor :title, :rating, :genre
+    def initalize(title, rating, genre)
+        @title = title
+        @rating = rating
+        @genre = genre
+    end
+end
+
+
+
+
+##### >>> Object marshaling for saving across terminal sessions
+# if File.file?('movies.dump')
+#     ### loads movies array if it exists
+#     $ports = Marshal.load(File.read('user.dump'))
+#     puts `clear`
+#     # userMenu()
+# else 
+#     ### else creates a new array
+#     $movies = []
+#     puts `clear`
+#     newUser()
+# end
+
+# # saves the array of movies to "movies.dump"
+# File.open('movies.dump', 'w') { |f| f.write(Marshal.dump($movies)) }
