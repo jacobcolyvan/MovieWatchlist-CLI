824876acfc0067749082d8bd6e6b4b1368b2ff63
hughgreethead <hugh.greethead@gmail.com>
2020-03-26 15:58:07 +1100

    added list of movies

--- control.rb
+++ control.rb
@@ -9,7 +9,25 @@ if File.file?('movies.dump')
     puts `clear`
 else 
     ### else creates a new array
-    $moviesWatchlist = [Movie.new("Godfather", "Drama")]
+    $moviesWatchlist = [Movie.new("The Shawshank Redemption", "Drama"), Movie.new("The Godfather", "Drama"), 
+                        Movie.new("The Dark Knight", "Action"), Movie.new("The Godfather: Part II", "Drama"),
+                        Movie.new("The Lord of the Rings: The Return of the King", "Adventure"),
+                        Movie.new("Pulp Fiction", "Crime"), Movie.new("Schindler's List", "Biography"),
+                        Movie.new("12 Angry Men", "Drama"), Movie.new("Inception", "Action"),
+                        Movie.new("Fight Club", "Drama"), Movie.new("The Lord of the Rings: The Fellowship of the Ring", "Adventure"),
+                        Movie.new("Forrest Gump", "Drama"),  Movie.new("The Good, the Bad and the Ugly", "Western"),
+                        Movie.new("The Lord of the Rings: The Two Towers", "Adventure"), Movie.new("The Matrix", "Action"),
+                        Movie.new("Goodfellas", "Biography"), Movie.new("Star Wars: Episode V - The Empire Strikes Back", "Adventure"),
+                        Movie.new("One Flew Over the Cuckoo's Nest", "Drama"), Movie.new("Harakiri", "Action"),
+                        Movie.new("Parasite", "Comedy"), Movie.new("Interstellar", "Sci-Fi"), Movie.new("City of God", "Crime"),
+                        Movie.new("Spirited Away", "Animation"), Movie.new("Saving Private Ryan", "Drama"), Movie.new("The Green Mile", "Crime"),
+                        Movie.new("Life Is Beautiful", "Drama"), Movie.new("Se7en", "Crime"), Movie.new("The Silence of the Lambs", "Crime"),
+                        Movie.new("Star Wars", "Adventure"), Movie.new("Seven Samurai", "Adventure"), Movie.new("Its a Wonderful Life", "Drama"),
+                        Movie.new("Joker", "Drama"), Movie.new("Avengers: Infinity War", "Adventure"), Movie.new("Whiplash", "Drama"),
+                        Movie.new("The Intouchables", "Biography"), Movie.new("The Prestige", "Drama"), Movie.new("The Departed", "Drama"),
+                        Movie.new("The Pianist", "Biography"), Movie.new("Gladiator", "Action"), Movie.new("American History X", "Drama"),
+                        Movie.new("The Usual Suspects", "Crime")
+                    ]
     puts `clear`
 end
 
